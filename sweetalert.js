ee4a84efd77a8ad093a105c959209e7036ea3677
Phxntxm <fabianmolina325@gmail.com>
2022-05-23 14:30:59 -0300

    se añade la libreria sweetalert

---
+++ sweetalert.js
@@ -0,0 +1,8 @@
+
+document.getElementById("submitButton").onclick = function () {
+    Swal.fire(
+        'Gracias por tu compra',
+        'Tu producto llegara pronto',
+        'success'
+      )
+}
